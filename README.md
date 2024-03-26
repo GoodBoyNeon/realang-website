b63755fc1cb9c344db6740b046d17a9271092237
JadeBetty <jadebetty.7261@gmail.com>
2024-03-26 17:18:24 +0700

    first commit yay

---
+++ README.md
@@ -0,0 +1,5 @@
+# Realang documentations
+
+
+This is the source code for the offical Realang website.
+Feel free to contribute.
---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["JetBrains Mono", "san-serif"],
+    },
+    colors: {
+      "dark": "#080C0D",
+      "t-dark": "#F6F8F9",
+      "white": "#BFC1B9",
+      "first-col": "#5F86A5",
+      "second-col": "#4B727A",
+      "third-col": "#6D8E7E",
+      "fourth-col": "#415A42",
+      "fifth-col": "1B2E3E",
+    },
+  },
+  plugins: [],
+};
